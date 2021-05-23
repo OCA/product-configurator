037e03a8e5a0282cb543a81f536b94692d50b02b
Julio c Bravo <jbr@wedoo.tech>
2021-05-22 22:07:31 -0500

    MOD Initializing the variable cfg_step_ids in wizard product configurator

--- product_configurator/wizard/product_configurator.py
+++ product_configurator/wizard/product_configurator.py
@@ -587,6 +587,9 @@ class ProductConfigurator(models.TransientModel):
         self, attr_lines, field_prefix, custom_field_prefix, dynamic_fields, wiz
     ):
 
+        #Initializing the variable cfg_step_ids
+        cfg_step_ids = []
+
         for attr_line in attr_lines:
 
             attribute_id = attr_line.attribute_id.id
