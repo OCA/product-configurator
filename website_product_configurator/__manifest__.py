46be0f4934262df43b632f341fc6b191e39d1c16
Daniel Reis <dreis@opensourceintegrators.com>
2021-12-10 12:13:26 +0000

    [REF] *: fix PCatinean maintainer

--- website_product_configurator/__manifest__.py
+++ website_product_configurator/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Website Product Configurator",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.1.1",
     "summary": """Configure products in e-shop""",
     "author": "Pledra, Odoo Community Association (OCA)",
     "license": "AGPL-3",
@@ -26,5 +26,5 @@
     "application": True,
     "installable": True,
     "development_status": "Beta",
-    "maintainers": ["pcatinean"],
+    "maintainers": ["PCatinean"],
 }
