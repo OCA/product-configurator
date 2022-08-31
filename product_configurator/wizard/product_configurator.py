df6ecd9555f39d71b747978821884e318e5af87e
Andrea Piovesana <andrea.m.piovesana@gmail.com>
2022-08-31 12:15:45 +0200

    Update product_configurator.py

--- product_configurator/wizard/product_configurator.py
+++ product_configurator/wizard/product_configurator.py
@@ -784,17 +784,15 @@ class ProductConfigurator(models.TransientModel):
             product = self.env["product.product"].browse(vals["product_id"])
             pta_value_ids = product.product_template_attribute_value_ids
             attr_value_ids = pta_value_ids.mapped("product_attribute_value_id")
+            vals.update(
+                {
+                    "product_tmpl_id": product.product_tmpl_id.id,
+                }
+            )
             if attr_value_ids:
                 vals.update(
                     {
-                        "product_tmpl_id": product.product_tmpl_id.id,
                         "value_ids": [(6, 0, attr_value_ids.ids)],
-                    }
-                )
-            else:
-                vals.update(
-                    {
-                        "product_tmpl_id": product.product_tmpl_id.id,
                     }
                 )
         # Get existing session for this product_template or create a new one
