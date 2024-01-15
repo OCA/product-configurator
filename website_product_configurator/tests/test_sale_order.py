c35a496d5b6528ca4b90ca8e08e1e81f3541b711
Simone Rubino <simone.rubino@aion-tech.it>
2024-01-15 12:59:59 +0100

    [MIG] website_product_configurator: Migration to 16.0

--- website_product_configurator/tests/test_sale_order.py
+++ website_product_configurator/tests/test_sale_order.py
@@ -100,29 +100,42 @@ class TestSaleOrder(TestProductConfiguratorValues):
             "If wrong value is added then Order Line quantity as it is.",
         )
 
-    def test_get_real_price_currency(self):
-        price, rule_id = self.sale_order.pricelist_id.get_product_price_rule(
-            self.sale_order.order_line.product_id,
-            self.sale_order.order_line.product_uom_qty,
-            self.sale_order.partner_id,
+    def test_cart_update_multi_attribute(self):
+        """A product having multiple values selected for a multi attribute
+        can be added to cart"""
+        # Arrange
+        session = self.session_id
+        session.value_ids |= (
+            self.env.ref("product_configurator.product_attribute_value_218i")
+            | self.env.ref("product_configurator.product_attribute_value_rims_378")
+            | self.env.ref(
+                "product_configurator.product_attribute_value_tapistry_black"
+            )
+            | self.env.ref("product_configurator.product_attribute_value_sport_line")
+            | self.env.ref("product_configurator.product_attribute_value_armrest")
+            | self.env.ref(
+                "product_configurator.product_attribute_value_smoker_package"
+            )
         )
-        self.sale_order.order_line._get_real_price_currency(
-            self.sale_order.order_line.product_id,
-            rule_id,
-            self.sale_order.order_line.product_uom_qty,
-            self.sale_order.order_line.product_uom,
-            self.sale_order.pricelist_id.id,
+        session.action_confirm()
+        product = session.product_id
+        # pre-condition: Product has multiple values for a multi attribute
+        multi_attribute_line = self.env.ref(
+            "product_configurator.product_attribute_line_2_series_options"
         )
-        self.assertFalse(
-            self.product.product_tmpl_id.config_ok, "product is config_ok True"
+        self.assertTrue(multi_attribute_line.multi)
+        multi_attribute = multi_attribute_line.attribute_id
+        multi_values = product.product_template_attribute_value_ids.filtered(
+            lambda ptav, attribute=multi_attribute: ptav.attribute_id == multi_attribute
         )
-        self.product.product_tmpl_id.write({"config_ok": True})
-        price, currency = self.sale_order.order_line._get_real_price_currency(
-            self.sale_order.order_line.product_id,
-            rule_id,
-            self.sale_order.order_line.product_uom_qty,
-            self.sale_order.order_line.product_uom,
-            self.sale_order.pricelist_id.id,
+        self.assertGreater(len(multi_values), 1)
+
+        # Act
+        result = self.sale_order._cart_update(
+            product.id,
+            add_qty=1,
         )
-        self.assertEqual(price, 0.0)
-        self.assertEqual(currency, self.env.ref("base.USD"))
+
+        # Assert
+        order_line = self.env["sale.order.line"].browse(result["line_id"])
+        self.assertEqual(order_line.product_id, product)
