2512667860fa5ec34952bbd17d605e2a8e64d0f6
Simone Rubino <simone.rubino@aion-tech.it>
2024-01-18 15:40:52 +0100

    [IMP] website_product_configurator_mrp: pre-commit execution

--- website_product_configurator_mrp/static/src/js/website_sale.js
+++ website_product_configurator_mrp/static/src/js/website_sale.js
@@ -1,17 +1,18 @@
-odoo.define("website_product_configurator_mrp.config_website_sale_mrp", function (
-    require
-) {
-    "use strict";
+odoo.define(
+    "website_product_configurator_mrp.config_website_sale_mrp",
+    function (require) {
+        "use strict";
 
-    var publicWidget = require("web.public.widget");
-    require("website_product_configurator.config_website_sale");
+        var publicWidget = require("web.public.widget");
+        require("website_product_configurator.config_website_sale");
 
-    publicWidget.registry.WebsiteSale.include({
-        _onProductReady: function () {
-            var $form = this.$form;
-            this.rootProduct.assembly = $form.find('select[name="assembly"]').val();
-            var res = this._super.apply(this, arguments);
-            return res;
-        },
-    });
-});
+        publicWidget.registry.WebsiteSale.include({
+            _onProductReady: function () {
+                var $form = this.$form;
+                this.rootProduct.assembly = $form.find('select[name="assembly"]').val();
+                var res = this._super.apply(this, arguments);
+                return res;
+            },
+        });
+    }
+);
