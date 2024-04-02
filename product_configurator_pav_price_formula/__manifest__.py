da71af2bc1187b02f86cb7973a5d65135b8eb545
Simone Rubino <simone.rubino@aion-tech.it>
2024-04-02 11:01:51 +0200

    [ADD] product_configurator_pav_price_formula

---
+++ product_configurator_pav_price_formula/__manifest__.py
@@ -0,0 +1,21 @@
+#  Copyright 2024 Simone Rubino - Aion Tech
+#  License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Configurator and Product Attribute value price formula",
+    "version": "16.0.1.0.0",
+    "category": "Sales/Sales",
+    "summary": "Bridge module between product configurator "
+    "and formula for product attribute values.",
+    "author": "Aion Tech, Odoo Community Association (OCA)",
+    "maintainers": [
+        "SirAionTech",
+    ],
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/product-configurator",
+    "depends": [
+        "product_attribute_value_price_formula",
+        "product_configurator",
+    ],
+    "auto_install": True,
+}
---
+++ product_configurator_pav_price_formula/readme/USAGE.rst
@@ -0,0 +1,3 @@
+A new variable is available in the attribute value's formula:
+
+- `config_session`: the current configuration session.
