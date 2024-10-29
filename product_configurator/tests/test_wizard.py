3f911c6521d7c857813b25d89fbbf650fe0e8836
Simone Rubino <simone.rubino@aion-tech.it>
2024-10-29 10:20:24 +0100

    [IMP] product_configurator: fields_view_get is deprecated

--- product_configurator/tests/test_wizard.py
+++ product_configurator/tests/test_wizard.py
@@ -342,10 +342,10 @@ class ConfigurationWizard(ProductConfiguratorTestCases):
 
     def test_13_fields_view_get(self):
         product_config_wizard = self._check_wizard_nxt_step()
-        product_config_wizard.fields_view_get()
+        product_config_wizard.get_view()
         product_config_wizard.with_context(
             wizard_id=product_config_wizard.id
-        ).fields_view_get()
+        ).get_view()
         # custom value
         # custom value
         self.attr_line_fuel.custom = True
@@ -408,7 +408,7 @@ class ConfigurationWizard(ProductConfiguratorTestCases):
         product_config_wizard_1.action_next_step()
         product_config_wizard_1.with_context(
             wizard_id=product_config_wizard_1.id
-        ).fields_view_get()
+        ).get_view()
 
     def test_14_unlink(self):
         product_config_wizard = self._check_wizard_nxt_step()
