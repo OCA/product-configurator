3a11cbcbecb1dbb2add731e64e1df659f7cdd5c8
HItesh Jadav <hitesh.j.erpharbor@gmail.com>
2026-01-02 15:52:34 +0530

    [18.0][MIG] product_configurator_purchase: Migration to v18.0

---
+++ product_configurator_purchase/models/purchase.py
@@ -0,0 +1,63 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    def action_config_start(self):
+        """Return action to start configuration wizard"""
+        configurator_obj = self.env["product.configurator.purchase"]
+        return configurator_obj.with_context(
+            default_order_id=self.id,
+            wizard_model="product.configurator.purchase",
+            allow_preset_selection=True,
+        ).get_wizard_action()
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    custom_value_ids = fields.One2many(
+        comodel_name="product.config.session.custom.value",
+        inverse_name="cfg_session_id",
+        related="config_session_id.custom_value_ids",
+        string="Custom Values",
+    )
+    config_ok = fields.Boolean(
+        related="product_id.config_ok", string="Configurable", readonly=True
+    )
+    config_session_id = fields.Many2one(
+        comodel_name="product.config.session", string="Config Session"
+    )
+
+    def reconfigure_product(self):
+        """Creates and launches a product configurator wizard with a linked
+        template and variant in order to re-configure a existing product. It is
+        esetially a shortcut to pre-fill configuration data of a variant"""
+        wizard_model = "product.configurator.purchase"
+        extra_vals = {
+            "order_id": self.order_id.id,
+            "order_line_id": self.id,
+            "product_id": self.product_id.id,
+        }
+        self = self.with_context(
+            default_order_id=self.order_id.id,
+            default_order_line_id=self.id,
+        )
+        return self.product_id.product_tmpl_id.create_config_wizard(
+            model_name=wizard_model, extra_vals=extra_vals
+        )
+
+
+    @api.depends(
+        "config_ok",
+        "config_session_id",
+    )
+    def _compute_price_unit_and_date_planned_and_name(self):
+        res = super(PurchaseOrderLine, self)._compute_price_unit_and_date_planned_and_name()
+        for line in self:
+            if line.config_ok and line.config_session_id:
+                line.price_unit = line.config_session_id.price
+        return res
---
+++ product_configurator_purchase/tests/__init__.py
@@ -0,0 +1,2 @@
+from . import test_purchase
+from . import test_purchase_order_line
